e000cba4b4edc23ca14d19b3f92373da89e029a6
kazuyaster <kazuyaster@gmail.com>
2013-11-30 23:12:39 +0900

    カスタムセルをセット

--- source/HatebuPocket/HatebuPocket/FeedItemManager.m
+++ source/HatebuPocket/HatebuPocket/FeedItemManager.m
@@ -8,8 +8,19 @@
 
 #import "FeedItemManager.h"
 #import "FeedItem.h"
+#import "FeedCell.h"
+
+static FeedItemManager *_sharedInstance = nil;
 
 @implementation FeedItemManager
+
++ (id)sharedManager{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedInstance = [[FeedItemManager alloc]init];
+    });
+    return _sharedInstance;
+}
 
 - (id)init{
     
@@ -74,12 +85,17 @@
     return _items.count;
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return [FeedCell cellHeight];
+}
+
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    static NSString *cellIdentifer = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifer forIndexPath:indexPath];
+    static NSString *cellIdentifer = @"FeedCell";
+    FeedCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifer forIndexPath:indexPath];
     
-    cell.textLabel.text = ((FeedItem*)_items[indexPath.row]).title;
+    cell.title.text = ((FeedItem*)_items[indexPath.row]).title;
+    cell.description.text = ((FeedItem*)_items[indexPath.row]).description;
     return cell;
 }
 
@@ -91,6 +107,55 @@
                 [_delegate performSelector:@selector(selected:) withObject:_items[indexPath.row]];
             }
         }
+    }
+}
+
+
+-(void)downloadedFeed:(NSNotification*)notification{
+    
+    NSMutableArray *items = [[NSMutableArray alloc]init];
+    
+    NSDictionary *feedDict = (NSDictionary*)notification;
+    
+    @try {
+        
+        NSArray *tmpItems = [feedDict valueForKeyPath:@"object.feed.rdf:RDF.item"];
+        
+        for (NSDictionary *tmpDict in tmpItems) {
+            
+            NSLog(@"%@",[tmpDict valueForKeyPath:@"title.text"]);
+            NSLog(@"%@",[tmpDict valueForKeyPath:@"content:encoded.text"]);
+            NSLog(@"%@",[tmpDict valueForKeyPath:@"dc:date.text"]);
+            NSLog(@"%@",[tmpDict valueForKeyPath:@"dc:subject.text"]);
+            NSLog(@"%@",[tmpDict valueForKeyPath:@"description.text"]);
+            NSLog(@"%@",[tmpDict valueForKeyPath:@"hatena:bookmarkcount.text"]);
+            NSLog(@"%@",[tmpDict valueForKeyPath:@"link.text"]);
+            NSLog(@"%@",[tmpDict valueForKeyPath:@"rdf:about"]);
+            
+            FeedItem *item = [[FeedItem alloc]initWithTitle:[tmpDict valueForKeyPath:@"title.text"]
+                                              encodeContent:[tmpDict valueForKeyPath:@"content:encoded.text"]
+                                                       date:[tmpDict valueForKeyPath:@"dc:date.text"]
+                                                    subject:[tmpDict valueForKeyPath:@"dc:subject.text"]
+                                                description:[tmpDict valueForKeyPath:@"description.text"]
+                                              bookmarkCount:[tmpDict valueForKeyPath:@"hatena:bookmarkcount.text"]
+                                                       link:[tmpDict valueForKeyPath:@"link.text"]
+                                                      about:[tmpDict valueForKeyPath:@"rdf:about"]];
+            [items addObject:item];
+        }
+        [[self mutableArrayValueForKey:@"items"] insertObjects:items
+                                                     atIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0, items.count)]];
+
+    }
+    @catch (NSException *exception) {
+        UIAlertView *alertまわりくどい = [[UIAlertView alloc]initWithTitle:exception.name
+                                                       message:exception.reason
+                                                      delegate:nil
+                                             cancelButtonTitle:nil
+                                             otherButtonTitles:@"OK", nil];
+        [alert show];
+    }
+    @finally {
+
     }
 }
 
