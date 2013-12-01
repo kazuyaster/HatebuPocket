344b51d48e84b7efe2ebd7e6c09e3f1af9de9e64
kazuyaster <kazuyaster@gmail.com>
2013-12-01 21:38:24 +0900

    階層の変更

--- source/HatebuPocket/HatebuPocket/Define.h
+++
@@ -1,9 +0,0 @@
-//
-//  Define.h
-//  HatebuPocket
-//
-//  Created by 小田 和哉 on 2013/11/23.
-//  Copyright (c) 2013年 小田 和哉. All rights reserved.
-//
-
-#define KODDownloadFeedNotification @"KODDownloadFeedNotification"
